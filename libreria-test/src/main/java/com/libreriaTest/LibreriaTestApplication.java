a47cd5ffe352e3cd3b64d272fe96d332dd41781f
PabloGonzalez12 <pablo.ga.cp@gmail.com>
2025-10-28 14:40:41 +0100

    feat: test for a library

---
+++ libreria-test/src/main/java/com/libreriaTest/LibreriaTestApplication.java
@@ -0,0 +1,13 @@
+package com.libreriaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaTestApplication.class, args);
+	}
+
+}
---
+++ libreria-test/src/main/java/com/libreriaTest/snowflakeclient/ProxyController.java
@@ -0,0 +1,24 @@
+package com.libreriaTest.snowflakeclient;
+
+import com.libreria.apiclient.ApiClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProxyController {
+
+    private final ApiClient apiClient;
+
+    public ProxyController(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    @PostMapping("/probar")
+    public ResponseEntity<String> probar(@RequestBody ProbarRequestDTO body) {
+        if (body == null || body.getUrl() == null || body.getUrl().isBlank()) {
+            return ResponseEntity.badRequest().body("Falta 'url' en el body");
+        }
+        ResponseEntity<String> resp = apiClient.get(body.getUrl());
+        return ResponseEntity.status(resp.getStatusCode()).body(resp.getBody());
+    }
+}
