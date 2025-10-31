07e420fc45283d1553193183e04509fcb0a97a3c
David <d1v3dgp@gmail.com>
2025-10-31 09:21:10 +0100

    feat: fixing merge, now is OK

--- w2m-disputes-back/src/main/java/es/w2m/finance/disputes/w2mdisputes/W2mDisputesApplication.java
+++ w2m-disputes-back/src/main/java/es/w2m/finance/disputes/w2mdisputes/W2mDisputesApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = {
+        "es.w2m.finance.disputes.libsnowflake.client",
+        "es.w2m.finance.disputes.w2mdisputes.infrastructure.client"
+})
 @SpringBootApplication
 public class W2mDisputesApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(W2mDisputesApplication.class, args);
     }
 
--- w2m-disputes-back/src/main/java/es/w2m/finance/disputes/w2mdisputes/infrastructure/adapter/output/client/SnowflakeClientAdapter.java
+++ w2m-disputes-back/src/main/java/es/w2m/finance/disputes/w2mdisputes/infrastructure/adapter/output/client/SnowflakeClientAdapter.java
@@ -1,18 +1,20 @@
 package es.w2m.finance.disputes.w2mdisputes.infrastructure.adapter.output.client;
+
+import es.w2m.finance.disputes.libsnowflake.client.SnowflakeApiClient;
 import es.w2m.finance.disputes.w2mdisputes.application.port.output.SnowflakePort;
-import es.w2m.finance.disputes.w2mdisputes.infrastructure.client.SnowflakeApiClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class SnowflakeClientAdapter implements SnowflakePort {
 
-    private final SnowflakeApiClient feign;
+    private final SnowflakeApiClient snowflakeApiClient;
 
     @Override
-    public Map<String, Object> fetchDispute(String id) {
-        return feign.getDispute();
+    public Map<String, Object> fetchDispute(final String id) {
+        return this.snowflakeApiClient.getDispute();
     }
 }
